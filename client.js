5707ea25fe9a34a31b2601c328ff0759e1f76509
FelixPriori <felixriouxsabourin@gmail.com>
2020-02-12 16:28:33 +0000

    played with moving up

--- client.js
+++ client.js
@@ -8,15 +8,29 @@ const connect = function() {
     host: '192.168.0.103',
     port: 50541
   });
+
   console.log('Connection established.');
+  conn.write('Name: SK8');
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
   conn.on('data', console.log);
-  conn.on('connect', () => {
-    conn.write('Name: SK8');
-  });
+
   return conn;
   
 }
 
 module.exports = {connect};
+
+// let timer = 0;
+// for(let i = 0; i < 50; i++) {
+//   setTimeout(() => {
+//     conn.write('Move: up'); 
+//   }, timer += 50);
+// }
+
+
+/*  
+  conn.write('Move: down');
+  conn.write('Move: right');
+  conn.write('Move: left');
+*/
