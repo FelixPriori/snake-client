acfc8cddb4075b7041765c0cf5dfbf8e2120d153
FelixPriori <felixriouxsabourin@gmail.com>
2020-02-12 16:17:09 +0000

    Added name

--- client.js
+++ client.js
@@ -5,14 +5,18 @@ const net = require('net');
  */
 const connect = function() {
   const conn = net.createConnection({ 
-    host: '10.0.2.15',
+    host: '192.168.0.103',
     port: 50541
   });
+  console.log('Connection established.');
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
   conn.on('data', console.log);
-
+  conn.on('connect', () => {
+    conn.write('Name: SK8');
+  });
   return conn;
+  
 }
 
 module.exports = {connect};
